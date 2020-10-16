a7782af9abd0f2c4e1b92c0526cbcc01ab2ff216
sanyamtoap <58058642+sanyamtoap@users.noreply.github.com>
2020-10-16 08:41:39 +0530

    Update dynasm-driver.c

--- dynasm-driver.c
+++ dynasm-driver.c
@@ -23,32 +23,32 @@ void *jitcode(dasm_State **state) {
   int dasm_status = dasm_link(state, &size);
   assert(dasm_status == DASM_S_OK);
 
-  // Allocate memory readable and writable so we can
-  // write the encoded instructions there.
+  /* Allocate memory readable and writable so we can
+     write the encoded instructions there. */
   char *mem = mmap(NULL, size + sizeof(size_t),
 		   PROT_READ | PROT_WRITE,
                    MAP_ANON | MAP_PRIVATE, -1, 0);
   assert(mem != MAP_FAILED);
 
-  // Store length at the beginning of the region, so we
-  // can free it without additional context.
+  /* Store length at the beginning of the region, so we
+     can free it without additional context. */
   *(size_t*)mem = size;
   void *ret = mem + sizeof(size_t);
 
   dasm_encode(state, ret);
   dasm_free(state);
 
-  // Adjust the memory permissions so it is executable
-  // but no longer writable.
+  /* Adjust the memory permissions so it is executable
+     but no longer writable. */
   int success = mprotect(mem, size, PROT_EXEC | PROT_READ);
   assert(success == 0);
 
 #ifndef NDEBUG
-  // Write generated machine code to a temporary file.
-  // View with:
-  //  objdump -D -b binary -mi386 -Mx86-64 /tmp/jitcode
-  // Or:
-  //  ndisasm -b 64 /tmp/jitcode
+  /* Write generated machine code to a temporary file.
+     View with:
+     objdump -D -b binary -mi386 -Mx86-64 /tmp/jitcode
+     Or:
+     ndisasm -b 64 /tmp/jitcode. */
   FILE *f = fopen("/tmp/jitcode", "wb");
   fwrite(ret, size, 1, f);
   fclose(f);
